31e8b0ce978b1f319adf7c47fcc3a5b4ed9b9175
Francisco de Juan <fjuan@splendeo.es>
2013-06-05 19:17:00 +0200

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Padel::Application.config.secret_token = 'bc9b95aef7c4f854ddab656dac68630708c2c41fbc3b3817836be186b75917662161342e872d7ffcf75d7b921bfdd455f400a269ffcb710610ebadb8c5dd7452'
