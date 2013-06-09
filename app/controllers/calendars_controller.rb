c5b475cf2ff2208d418d0a14cde059fcfb9f37d9
Francisco de Juan <fjuan@splendeo.es>
2013-06-09 18:51:15 +0200

    add translation and calendar navigation to calendar view and controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,5 +2,12 @@ class CalendarsController < ApplicationController
   def show
     @users_by_availability = User.by_availability
     @weekdays = %w(monday tuesday wednesday thursday friday saturday sunday)
+
+    @date = params[:date] && Date.parse(params[:date]) || Date.today
+
+    @beginning_of_week = @date.beginning_of_week
+    @end_of_week       = @date.end_of_week
+
+    @weekdays = (@beginning_of_week..@end_of_week)
   end
 end
