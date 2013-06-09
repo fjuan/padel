834fb588fad11402bc1948b4b2f9aa1302228066
Francisco de Juan <fjuan@splendeo.es>
2013-06-09 18:22:11 +0200

    Configure default locale :es. Add translations for account, devise and rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate_user!
+  before_filter :set_locale
+
+
+  private
+  
+  def set_locale
+    I18n.locale = :es
+  end 
 end
